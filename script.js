fa7a39e6b7481b5ff0a363d291ba11e87321104d
Sebastian Szaładyga <sebastian.szaladyga@gmail.com>
2023-06-08 12:20:30 +0200

    games interaction

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ let highscore = 0;
 const textMessage = function (message) {
     document.querySelector('.message').inner.Text = message;
 }
+
+document.querySelector('.check').addEventListener('click', function () {
+    const guess = Number(document.querySelector('.guess').value);
+
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct number!';
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.background = '#60b347';
+        
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+        
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            textMessage(guess > secretNumber ? 'Too high!' : 'Too low!')
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            textMessage('You lost the game');
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
